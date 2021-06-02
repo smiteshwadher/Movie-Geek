aceaf02b40a8179e7bc925402a56558d4d29b890
Smitesh Wadher <smiteshwadher@gmail.com>
2021-06-03 00:42:01 +0530

    Initial Commit

---
+++ Movie-Geek/Movie_GeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_GeekApp.swift
+//  Movie-Geek
+//
+//  Created by Smitesh Wadher on 03/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Movie_GeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
